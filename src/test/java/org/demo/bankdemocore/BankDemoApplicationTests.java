bc5087f848ad7e3eeec7b059a8ca8e9429983e9b
MarcoBackman <roni2006@hanmail.net>
2024-04-28 17:38:23 -0400

    Test context load bypass

--- src/test/java/org/demo/bankdemocore/BankDemoApplicationTests.java
+++ src/test/java/org/demo/bankdemocore/BankDemoApplicationTests.java
@@ -1,9 +1,16 @@
 package org.demo.bankdemocore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontProperties;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@ActiveProfiles(value = "default,test")
+@TestPropertySource("classpath:application-test.properties")
+@EnableAutoConfiguration(exclude= SecurityAutoConfiguration.class)
 class BankDemoApplicationTests {
 
     @Test
